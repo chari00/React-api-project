5a999ffc657d5783320f1befa065cef679fad290
chari00 <macharitocruzuk@gmail.com>
2023-03-16 16:48:50 +0000

    add map.js and css style to map component.

---
+++ src/components/MapTab/Map.js
@@ -0,0 +1,25 @@
+import { useMemo } from "react";
+import {
+  GoogleMap,
+  
+} from "@react-google-maps/api";
+import '../MapTab/map.css'
+
+export default function Map() {
+  const center = useMemo(() => ({ lat: 51, lng: 0 }), []);
+  
+  return (
+    <div className="container">
+      <div className="controls">
+        <h3>Map</h3>
+      </div>
+      <div className="map">
+        <GoogleMap
+          zoom={8}
+          center={ center }
+          mapContainerClassName="map-container"
+        ></GoogleMap>
+      </div>
+    </div>
+  );
+}
