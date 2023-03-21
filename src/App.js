b66c653b3f21334ff17ccb6371819cbf785fe0c9
George <georgeoustapas@gmail.com>
2023-03-21 20:21:57 +0000

    fixed issue with movie-glue api fetch

--- src/App.js
+++ src/App.js
@@ -3,34 +3,7 @@ import { useState, useEffect } from 'react';
 
 
 function App() {
-  // let APIKey = "f44cfcc45327fddc443fa6767211b14b";
-  let APIKey = "31b1715eea27e8546c5192709d456eb7";
-
-  const [long,setLong] = useState()
-  const [lat,setLat] = useState()
-
-
-
-  const [location, setLocation ] = useState("")
-  // const [city,setCity] = useState()
-  const FetchLocation = () => {
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q="+ location +"&limit=5&appid=" + APIKey)
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        // setLocation("data")
-        setLat(data[0].lat)
-        setLong(data[0].lon)
-        console.log(lat);
-        console.log(long);
-
-      })
-  }
-  useEffect(() => {
-    FetchLocation()
-  }, [location])
-
+ 
   
  
   return (
